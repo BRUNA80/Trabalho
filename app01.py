f93ffa0118bba554ac95cad9d4c62f7b824b0a77
Bruna <brunaacostaa.rocha@gmail.com>
2024-10-15 11:49:37 -0300

    Create app01.py
    aula

---
+++ app01.py
@@ -0,0 +1,23 @@
+#criar uma variavel 
+...
+
+#Criar uma váriavel usando #snake_case
+cor_da_casa= 'azul'
+
+#Cria uma váriavel usando "camelcase"
+CorDaCasa = "amarela"
+
+#Primeiro nome
+First_name= 'Bruna'
+
+#Ultimo nome
+last_name= Rocha 
+
+#Imprime meu nome
+print (First_name)
+print (last_name)
+
+#exibe as coisas 
+print ('\n\n\n')
+print (cor_da_casa)
+print (CorDaCasa)
